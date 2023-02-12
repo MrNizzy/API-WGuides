f24faaaf06c9e2f1edb96510d632b4aa5330dcf4
BuildTools <unconfigured@null.spigotmc.org>
2023-02-12 12:37:01 -0500

    Add: New routes

--- app.routes.js
+++ app.routes.js
@@ -14,6 +14,9 @@ module.exports = function(db){
     router.use(require('./routes/equipment')(db));
     router.use(require('./routes/statistics')(db));
     router.use(require('./routes/talents')(db));
+    router.use(require('./routes/gems')(db));
+    router.use(require('./routes/glyphs')(db));
+    router.use(require('./routes/races')(db));
 
     return router
 }
---
+++ routes/glyphs.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = function(db) {
+    router.get('/api/guides/glyphs/:id', (req, res) => {
+        db.all('SELECT * FROM glyphs_class WHERE id_reference = ?', [req.params.id], (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            if (rows.length > 0) {
+                res.send(rows);
+            } else {
+                res.send('No glyphs found');
+            }
+        });
+    });
+
+    return router;
+}
